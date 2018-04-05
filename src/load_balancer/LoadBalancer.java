fc4d6282d8995b643cebc8496270722375d446db
orion40 <qolizion@protonmail.com>
2018-04-05 14:54:39 +0200

    Add more TODOs to LoadBalancer

--- src/load_balancer/LoadBalancer.java
+++ src/load_balancer/LoadBalancer.java
@@ -1,5 +1,10 @@
 
 public class LoadBalancer {
+
+    private static String hashString;
+    private static String hostFile;
+    private static String dictionnaryFile;
+
     public static void main(String [] args)
     {
         // TODO: launch all the servers
@@ -8,7 +13,38 @@ public class LoadBalancer {
 
         // then we switch to lsiten mode for clients to connect
 
+        if (args.length < 3){
+            System.out.println("The load balancer need a MD5 hash, a host file and a dictionnary as argument.");
+            System.exit(0);
+        }
+
+        hashString = args[0];
+        hostFile = args[1];
+        dictionnaryFile = args[2];
+
+        System.out.println("hash: " + hashString + " hostFile: " + hostFile + " dictionnaryFile: " + dictionnaryFile);
+
+        splitDictionnary();
+        launchServers();
+        distributeDictionnary();
+        waitForClients();
     }
 
+    private static void launchServers(){
+        // TODO: we go through host file, and we launch each servers
+    }
+
+    private static void splitDictionnary(){
+        // TODO: open and split dictionnary file
+    }
+
+    private static void distributeDictionnary(){
+        // TODO: put the file parts into a rabbitMQ queue
+        // when the queue is empty, the function is over
+    }
+
+    private static void waitForClients(){
+        // TODO: wait for a client to connect, use a rabiitMQ queue ?
+    }
 }
 
