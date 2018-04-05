8898ed42ed955e81c5b6d2f98dc0310e6fe1d129
orion40 <qolizion@protonmail.com>
2018-04-05 15:16:44 +0200

    Add more todo

--- src/client/Client.java
+++ src/client/Client.java
@@ -6,5 +6,27 @@ public class Client {
         // do the work
         // send results
         // quit once no more work or result found
+
+        if (args.length < 1){
+            System.out.println("This need an IP to connect to.");
+        }
+
+        connectToLoadBalancer();
+        while(workToDo && !resultFound){
+            getWork();
+            sendResults();
+        }
+    }
+
+    private static void connectToLoadBalancer(){
+        // TODO: connect to the load balancer
+    }
+
+    private static void getWork(){
+        // TODO: request work from server
+    }
+
+    private static void sendResults(){
+        // TODO: send result to out server
     }
 }
--- src/server/HashServer.java
+++ src/server/HashServer.java
@@ -1,9 +1,31 @@
 public class HashServer {
 
-    public static void main (String[] args){
+    public static void main (String[] args) throws InterruptedException{
         // TODO: bootstrap
         // we get the dictionnary from the load balancer
         //
         // then we wait for incoming connections
+        // servers communicate between themselves with rings
+
+        getDictionnaryPart();
+        waitForClients();
+        propagateResults();
+    }
+
+    public static void getDictionnaryPart(){
+        // TODO: get dictionnary part from the load balancer
+    }
+
+    public static void waitForClients(){
+        // TODO: wait for connection, when the work is done,
+        // or when someone found the result, we propagate them
+        while (true){
+            System.out.println("[SERVER] Waiting...");
+            Thread.sleep(1000);
+        }
+    }
+
+    public static void propagateResults(){
+        // TODO: we send to the other servers the result of our clients
     }
 }
