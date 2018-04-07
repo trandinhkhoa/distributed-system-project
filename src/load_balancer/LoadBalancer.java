063c81c7ed0c6ab23c3e0ddd51f230efb093dc21
orion40 <qolizion@protonmail.com>
2018-04-07 16:56:49 +0200

    Add basic dictionnary handling

--- src/load_balancer/LoadBalancer.java
+++ src/load_balancer/LoadBalancer.java
@@ -1,9 +1,27 @@
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+import java.util.ArrayList;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.util.Stack;
 
 public class LoadBalancer {
 
     private static String hashString;
     private static String hostFile;
     private static String dictionnaryFile;
+
+    private static int bigChunkSize;
+    private static int numberOfWords;
+    private static int numberOfServers;
+
+    private static Stack<Stack<String>> bigChunks = new Stack<>();
+
+    private static ArrayList<String> serverList = new ArrayList<>();
 
     public static void main(String [] args)
     {
@@ -24,18 +42,70 @@ public class LoadBalancer {
 
         System.out.println("hash: " + hashString + " hostFile: " + hostFile + " dictionnaryFile: " + dictionnaryFile);
 
-        splitDictionnary();
-        launchServers();
+        try {
+            launchServers();
+        } catch (FileNotFoundException e){
+            System.err.println("Hostfile not found.\n" + e.getMessage());
+            System.exit(1);
+        } catch (IOException e){
+            System.err.println("Unable to read from file:\n" + e.getMessage());
+            System.exit(1);
+        }
+
+        try {
+            splitDictionnary();
+        } catch (FileNotFoundException e){
+            System.err.println("Dictionnary file not found.\n" + e.getMessage());
+            System.exit(1);
+        } catch (IOException e){
+            System.err.println("Unable to read from file:\n" + e.getMessage());
+            System.exit(1);
+        }
+
         distributeDictionnary();
         waitForClients();
     }
 
-    private static void launchServers(){
+    private static void launchServers() throws FileNotFoundException, IOException{
         // TODO: we go through host file, and we launch each servers
+        System.out.println("[LB] Reading server file...");
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(hostFile));
+        String serverIp = bufferedReader.readLine();
+        while (serverIp != null){
+            serverList.add(serverIp);
+            System.out.println(serverIp);
+            serverIp = bufferedReader.readLine();
+        }
+        numberOfServers = serverList.size();
     }
 
-    private static void splitDictionnary(){
+    private static void splitDictionnary() throws IOException{
         // TODO: open and split dictionnary file
+        BufferedReader reader = new BufferedReader(new FileReader(dictionnaryFile));
+        System.out.println("Computing number of words...");
+        while (reader.readLine() != null) numberOfWords++;
+        reader.close();
+        System.out.println("num of words:" + numberOfWords);
+        bigChunkSize = numberOfWords/numberOfServers;
+        System.out.println("Chunksize : " + bigChunkSize);
+
+        System.out.println("Creating chunks...");
+
+        reader = new BufferedReader(new FileReader(dictionnaryFile));
+        String s = reader.readLine();
+        Stack<String> stack = new Stack<>();
+        while (s != null){
+            if (stack.size() < bigChunkSize)
+                stack.add(s);
+            else{
+                bigChunks.add(stack);
+                stack = new Stack<>();
+                stack.add(s);
+            }
+            // TODO: limit password size in the dictionnary ?
+            s = reader.readLine();
+        }
+        bigChunks.add(stack);
     }
 
     private static void distributeDictionnary(){
