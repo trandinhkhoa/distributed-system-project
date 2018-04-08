5fe5ebb7c7b92a46a677af5d9f2c7c954eb69934
orion40 <qolizion@protonmail.com>
2018-04-08 12:04:30 +0200

    Add usage function and arguments for servers

--- src/launch.sh
+++ src/launch.sh
@@ -1,12 +1,19 @@
-if [ $# -ne 1 ] ; then
-    echo "This script require the path of an hostfile as argument.";
+if [ $# -ne 3 ] ; then
+    echo "Usage: script.sh <host file> <hash> <rabbitMQ queue IP>";
+    echo -e "\tThis script launch the servers provided by the hostfile and give them";
+    echo -e "\tthe hash to compute.";
+    echo -e "\t<host file> The host file to read from";
+    echo -e "\t<hash> The hash to compute";
+    echo -e "\t<rabbitMQ queue IP> The IP of the loadbalancer";
     exit 0;
 fi
 
 server_path=/$(pwd)/
 username=$(whoami)
+hash=$2
+rabbitmq_ip=$3
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
         echo "Text read from file: $line";
-        ssh $username@$line "cd $server_path; pwd ; ant server" &
+        ssh $username@$line "cd $server_path; pwd ; ant server -Dhash=\"$hash\"" &
     done < "$1"
