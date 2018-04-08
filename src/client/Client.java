ad925f7ab5bfce2d6ed848b7159ac4559c86f5a2
orion40 <qolizion@protonmail.com>
2018-04-08 12:06:12 +0200

    Add IPv4 verification, and some instructions

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,6 +1,11 @@
+import java.util.Stack;
+import org.apache.commons.validator.routines.InetAddressValidator;
+
 public class Client {
     private static boolean workToDo;
     private static boolean resultFound;
+
+    private static Stack<String> chunk;
 
     public static void main (String[] args){
         // TODO: connect to load balancer
@@ -10,12 +15,23 @@ public class Client {
         // quit once no more work or result found
 
         if (args.length < 1){
-            System.out.println("This need an IP to connect to.");
+            System.out.println("The client need an IP to connect to.");
+            System.exit(0);
         }
+
+        InetAddressValidator addressValidator = new InetAddressValidator();
+
+        if (addressValidator.getInstance().isValidInet4Address(args[0]) == false){
+            System.out.println("[Client] Please enter a proper IP.");
+            System.exit(1);
+        }
+
+        System.out.println(args[0]);
 
         connectToLoadBalancer();
         while(workToDo && !resultFound){
             getWork();
+            doWork();
             sendResults();
         }
     }
@@ -28,9 +44,26 @@ public class Client {
 
     private static void getWork(){
         // TODO: request work from server
+        // If the server send NULL, we have no more work to do.
+        // If the server send something, we add it to our chunk.
+    }
+
+    private static void doWork(){
+        // TODO: do the hash computing
+        /*
+        if (...){
+            resultFound = true;
+        }
+        */
+
     }
 
     private static void sendResults(){
         // TODO: send result to out server
+        if (resultFound == true){
+            // TODO: send the result to the server
+        } else {
+            // TODO: request more work; not needed since we loop ?
+        }
     }
 }
