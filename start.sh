c6b11b5eb0520d81609e07eb594d1513823d8fcd
orion40 <qolizion@protonmail.com>
2018-04-12 11:55:55 +0200

    Add argument for number of servers

--- start.sh
+++ start.sh
@@ -1,25 +1,28 @@
-if [ $# -eq 0 ] ; then
-    echo "Usage: start.sh <arg>";
-    echo -e "\tserver"
-    echo -e "\tclient"
-    echo -e "\tload_balancer <hashString> <path/to/dict>"
-    exit 0;
-fi
+usage(){
+    if [ $# -eq 0 ] ; then
+        echo "Usage: start.sh <arg>";
+        echo -e "\tserver"
+        echo -e "\tclient"
+        echo -e "\tload_balancer <MD5 hash string> <path/to/dict> <number of servers>"
+        exit 0;
+    fi
+}
 
-ant compile
 if [ "$1" == "server" ] ; then
+    ant compile
     cd build/
     java -cp ../lib/amqp-client-5.1.2.jar:../lib/slf4j-api-1.7.21.jar:../lib/slf4j-simple-1.7.22.jar:. HashServer
 elif [ "$1" == "client" ] ; then
+    ant compile
     cd build/
     java -cp ../lib/amqp-client-5.1.2.jar:../lib/slf4j-api-1.7.21.jar:../lib/slf4j-simple-1.7.22.jar:. Client
 elif [ "$1" == "load_balancer" ] ; then
-    if [ $# -ne 3 ]; then
-        echo "Load balancer require a hash string and a path to a dictionary.";
-        exit 1;
+    if [ $# -ne 4 ]; then
+        usage;
     fi
+    ant compile
     cd build/
-    java -cp ../lib/amqp-client-5.1.2.jar:../lib/slf4j-api-1.7.21.jar:../lib/slf4j-simple-1.7.22.jar:. LoadBalancer $2 $3
+    java -cp ../lib/amqp-client-5.1.2.jar:../lib/slf4j-api-1.7.21.jar:../lib/slf4j-simple-1.7.22.jar:. LoadBalancer $2 $3 $4
 else
     echo "Wrong argument.";
 fi
